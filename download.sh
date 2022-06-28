a1245cd1c8b3efc55534f3ff605d3bd946f9beba
franzer <awfulrofl@gmail.com>
2022-06-28 14:54:11 +0000

    updating to include incremental

--- download.sh
+++ download.sh
@@ -3,4 +3,8 @@
 FILE="$(curl -I -Ls -o /dev/null -w %{url_effective} https://shdw-drive.genesysgo.net/snapshots/latest)"
 basename "$FILE"
 f="$(basename -- $FILE)"
-wget -O $f https://shdw-drive.genesysgo.net/snapshots/latest
+FILE2="$(curl -I -Ls -o /dev/null -w %{url_effective} https://shdw-drive.genesysgo.net/snapshots/latest-incremental)"
+basename "$FILE2"
+f2="$(basename -- $FILE2)"
+wget -O $f https://shdw-drive.genesysgo.net/snapshots/latest &
+wget -O $f2 https://shdw-drive.genesysgo.net/snapshots/latest-incremental
